6341ac800001dbfab5e0882bfc82028805ca6509
robo <robolab1902@naver.com>
2021-11-24 07:29:13 +0900

    해쉬 문제 1번 수정

--- 1주차/김주영/Main_Hash_001.java
+++ 1주차/김주영/Main_Hash_001.java
@@ -2,11 +2,16 @@ import java.util.Arrays;
 
 public class Main_Hash_001 {
 	public static String solution(String[] participant, String[] completion) {
+		// 입력받는 두 배열 sort를 통해 정렬
+		// 알파벳 순으로 정렬되게 됨.
 		Arrays.sort(participant);
         Arrays.sort(completion);
-
+        
         String answer = null;
         int i = 0;
+        
+        // 하나만 다르고 똑같은 요소를 가진 배열들이 알파벳 순으로 정렬됌.
+        // 둘 배열 순서가 똑같게 되어 다른 완주자가 있을경우 캐치
         while (i < completion.length) {
             if (!participant[i].equals(completion[i])) {
                 answer = participant[i];
@@ -16,6 +21,7 @@ public class Main_Hash_001 {
         }
 
         return participant[i];
+        
 	}
 	public static void main(String[] args) {
 		String[] participant = {"leo", "kiki", "eden"};
