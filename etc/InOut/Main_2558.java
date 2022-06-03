6c5fe1693385a70f488f1b93a5ac05f217f60954
robo <robolab1902@naver.com>
2022-06-03 22:59:31 +0900

    2557, 2558 등 입출력 능력 확인

---
+++ etc/InOut/Main_2558.java
@@ -0,0 +1,14 @@
+package InOut;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2558 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.println(a+b);
+	}
+}
